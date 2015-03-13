ff1be53cf821c5b11bbf0f598f85aa5ab1cbb5c3
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2015-03-14 00:08:52 +0100

    * create submodule `db` with some database stuff * add working query example * minor changes

---
+++ src/db/mod.rs
@@ -0,0 +1,63 @@
+use postgres;
+use openssl::ssl::{SslContext, SslMethod};
+use rustless;
+use r2d2;
+use r2d2_postgres::PostgresConnectionManager;
+
+// Some type aliases for nicer names
+pub type PooledDB = r2d2::Pool<PostgresConnectionManager>;
+pub type PooledDBConn<'a> = 
+    r2d2::PooledConnection<'a, PostgresConnectionManager>;
+
+
+// Will try to connect to the database specified by url
+pub fn setup(url: &str, pool_size: u32) -> Option<PooledDB> {
+    // Try to initialize SSL context
+    let ssl_mode = match SslContext::new(SslMethod::Sslv23) {
+        Ok(val) => postgres::SslMode::Prefer(val),
+        Err(e) => {
+            println!("ERROR: Creating SSL-context failed!");
+            println!("    => {}", e);
+            return None;
+        }
+    };
+
+    // Create manager
+    let manager = PostgresConnectionManager::new(url, ssl_mode);
+
+    // Configure, create and return pool
+    let config = r2d2::Config::builder().pool_size(pool_size).build();
+    let handler = Box::new(r2d2::NoopErrorHandler);
+
+    match r2d2::Pool::new(config, manager, handler) {
+        Ok(pool) => Some(pool),
+        Err(e) => {
+            println!("ERROR: Connection to database failed!");
+            println!("    => {}", e);
+            return None;
+        }
+    }
+}
+
+// Info: The following code is essentially from the rustless example.
+// To have easy access to the postgres connection in our endpoints, we want to
+// store the connection in the typemap `rustless::Application::ext`. In order
+// to use the typemap, we have to setup a key-value pair where the value is our
+// postgres connection type. We use the following empty struct as key type.
+pub struct AppDB;
+
+impl ::typemap::Key for AppDB {
+    type Value = PooledDB;
+}
+
+// To make access even easier, we can add a method to rustless::Application.
+// Endpoints just have to call `.db()` instead of `.get<AppDB>()`.
+pub trait DatabaseExt: rustless::Extensible {
+    fn db(&self) -> PooledDBConn;
+}
+impl DatabaseExt for rustless::Application {
+    fn db(&self) -> PooledDBConn {
+        // FIXME: Handle get() error
+        self.ext.get::<AppDB>().unwrap().get().unwrap()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,24 @@
 
 
 extern crate iron;
-extern crate postgres;
-extern crate rustless;
 extern crate openssl;
+extern crate postgres;
+extern crate r2d2;
+extern crate r2d2_postgres;
 extern crate "rustc-serialize" as rustc_serialize;
+extern crate rustless;
+extern crate typemap;
 
+use db::DatabaseExt;
 use iron::Iron;
-use openssl::ssl::{SslContext, SslMethod};
 use postgres::{Connection, SslMode};
 use rustless::{Application, Api, Nesting, Versioning};
 use rustc_serialize::json;
 use std::borrow::Borrow;
+    use std::str::FromStr;
+
+
+mod db;
 
 #[derive(RustcDecodable, RustcEncodable)]
 struct Canteen {
@@ -22,7 +29,7 @@ struct Canteen {
     city_id: i32,
     city_name: String,
     // GeoLocation common.Location
-    distance: i32,
+    distance: f64,
 }
 
 
@@ -34,43 +41,31 @@ fn main() {
             return;
         }
     };
-    let ssl_mode = match SslContext::new(SslMethod::Sslv23) {
-        Ok(val) => SslMode::Prefer(val),
-        Err(e) => {
-            println!("ERROR: Creating SSL-context failed!");
-            println!("    => {}", e);
-            return;
-        }
+    
+    let db = match db::setup(db_url.borrow(), 1) {
+        Some(result) => result,
+        None => return,
     };
 
-    let db = match Connection::connect(db_url.borrow(), &ssl_mode) {
-        Ok(val) => val,
-        Err(e) => {
-            println!("ERROR: Connection to database failed!");
-            println!("    => {}", e);
-            return;
-        }
-    };
+    // let stmt = db.prepare(
+    //     r#"SELECT canteens.id, canteens.name, city_id, cities.name, 
+    //     CAST(-1 AS float8) AS "distance"
+    //     FROM canteens
+    //     INNER JOIN cities ON cities.id=city_id
+    //     WHERE canteens.id=2"#).unwrap();
 
-    let stmt = db.prepare(
-        r#"SELECT canteens.id, canteens.name, city_id, cities.name, 
-        -1 AS "distance"
-        FROM canteens
-        INNER JOIN cities ON cities.id=city_id
-        WHERE canteens.id=2"#).unwrap();
-
-    let mut vec = std::vec::Vec::new();
-    for row in stmt.query(&[]).unwrap() {
-        let canteen = Canteen {
-            id: row.get(0),
-            name: row.get(1),
-            city_id: row.get(2),
-            city_name: row.get(3),
-            distance: row.get(4),
-        };
-        vec.push(canteen);
-    }
-    println!("{}", json::encode(&vec).unwrap());
+    // let mut vec = std::vec::Vec::new();
+    // for row in stmt.query(&[]).unwrap() {
+    //     let canteen = Canteen {
+    //         id: row.get(0),
+    //         name: row.get(1),
+    //         city_id: row.get(2),
+    //         city_name: row.get(3),
+    //         distance: row.get(4),
+    //     };
+    //     vec.push(canteen);
+    // }
+    // println!("{}", json::encode(&vec).unwrap());
 
     let api = Api::build(|api| {
         // Specify API version
@@ -79,24 +74,50 @@ fn main() {
 
         api.get("info/:id", |endpoint| {
             endpoint.handle(|client, params| {
-                client.text(params.find("id")
-                    .unwrap().as_string().unwrap().to_string())
+                let db = client.app.db();
+
+                let stmt = db.prepare(
+                    r#"SELECT canteens.id, canteens.name, city_id, cities.name, 
+                    CAST(-1 AS float8) AS "distance"
+                    FROM canteens
+                    INNER JOIN cities ON cities.id=city_id
+                    WHERE canteens.id=$1"#).unwrap();
+
+                let mut vec = std::vec::Vec::new();
+                let id : i32 = FromStr::from_str(
+                    params.find("id").unwrap().as_string().unwrap()).unwrap();
+                for row in stmt.query(&[&id]).unwrap() {
+                    let canteen = Canteen {
+                        id: row.get(0),
+                        name: row.get(1),
+                        city_id: row.get(2),
+                        city_name: row.get(3),
+                        distance: row.get(4),
+                    };
+                    vec.push(canteen);
+                }
+                client.text(json::encode(&vec).unwrap())
+                // client.text(params.find("id")
+                //     .unwrap().as_string().unwrap().to_string())
             })
         });
        
     });
 
-    let app = Application::new(api);
+    let mut app = Application::new(api);
+    app.ext.insert::<db::AppDB>(db);
 
     println!("Listening on port {}", port);
     Iron::new(app).http(("localhost", port)).unwrap();
 }
 
 
+// Fetches all needed environment variables
 fn fetch_env() -> Result<(u16, String), String> {
     use std::str::FromStr;
     use std::env;
 
+    // Try to get 'PORT' and convert it to u16
     let port = match env::var("PORT") {
         Err(_) => return Err("Env-Var 'PORT' is not set!".to_string()),
         Ok(val) => match FromStr::from_str(val.borrow()) {
@@ -106,6 +127,7 @@ fn fetch_env() -> Result<(u16, String), String> {
         },
     };
 
+    // Try to get the database connection string
     let db_url = match env::var("DATABASE_URL") {
         Err(_) => return Err("Env-Var 'DATABASE_URL' is not set!".to_string()),
         Ok(val) => val,
