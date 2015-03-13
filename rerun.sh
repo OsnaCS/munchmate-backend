d4addb6c31fa6db3bf004709b0d3fa9fcf0b7cc3
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2015-03-13 20:57:51 +0100

    Add database connection and example

--- rerun.sh
+++ rerun.sh
@@ -5,5 +5,6 @@ if [ $? = 0 ]
 then
     echo "--------- Restarting Webserver ---------"
     killall -q munchmate-backend;
+    . ./set_env.sh
     PORT=1338 ./target/debug/munchmate-backend &
 fi
--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,75 @@
 // #![allow(dead_code)]
 
 
-extern crate rustless;
-extern crate hyper;
 extern crate iron;
-extern crate valico;
+extern crate postgres;
+extern crate rustless;
+extern crate openssl;
+extern crate "rustc-serialize" as rustc_serialize;
 
 use iron::Iron;
-use rustless::{
-    Application, Api, Nesting, Versioning
-};
-use std::str::FromStr;
+use openssl::ssl::{SslContext, SslMethod};
+use postgres::{Connection, SslMode};
+use rustless::{Application, Api, Nesting, Versioning};
+use rustc_serialize::json;
 use std::borrow::Borrow;
 
+#[derive(RustcDecodable, RustcEncodable)]
+struct Canteen {
+    id: i32,
+    name: String,
+    city_id: i32,
+    city_name: String,
+    // GeoLocation common.Location
+    distance: i32,
+}
 
 
 fn main() {
-
-    let port = match fetch_env() {
+    let (port, db_url) = match fetch_env() {
         Ok(result) => result,
         Err(e) => {
             println!("ERROR: {}", e);
             return;
         }
     };
+    let ssl_mode = match SslContext::new(SslMethod::Sslv23) {
+        Ok(val) => SslMode::Prefer(val),
+        Err(e) => {
+            println!("ERROR: Creating SSL-context failed!");
+            println!("    => {}", e);
+            return;
+        }
+    };
+
+    let db = match Connection::connect(db_url.borrow(), &ssl_mode) {
+        Ok(val) => val,
+        Err(e) => {
+            println!("ERROR: Connection to database failed!");
+            println!("    => {}", e);
+            return;
+        }
+    };
+
+    let stmt = db.prepare(
+        r#"SELECT canteens.id, canteens.name, city_id, cities.name, 
+        -1 AS "distance"
+        FROM canteens
+        INNER JOIN cities ON cities.id=city_id
+        WHERE canteens.id=2"#).unwrap();
+
+    let mut vec = std::vec::Vec::new();
+    for row in stmt.query(&[]).unwrap() {
+        let canteen = Canteen {
+            id: row.get(0),
+            name: row.get(1),
+            city_id: row.get(2),
+            city_name: row.get(3),
+            distance: row.get(4),
+        };
+        vec.push(canteen);
+    }
+    println!("{}", json::encode(&vec).unwrap());
 
     let api = Api::build(|api| {
         // Specify API version
@@ -47,7 +93,8 @@ fn main() {
 }
 
 
-fn fetch_env() -> Result<u16, String> {
+fn fetch_env() -> Result<(u16, String), String> {
+    use std::str::FromStr;
     use std::env;
 
     let port = match env::var("PORT") {
@@ -59,5 +106,10 @@ fn fetch_env() -> Result<u16, String> {
         },
     };
 
-    Ok(port)
+    let db_url = match env::var("DATABASE_URL") {
+        Err(_) => return Err("Env-Var 'DATABASE_URL' is not set!".to_string()),
+        Ok(val) => val,
+    };
+
+    Ok((port, db_url))
 }
